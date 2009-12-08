ca6a1e4c38a6c97f55cac4dfadc12bd24a112296
piv <piv@4df3d6c7-0a05-0410-9bee-ae8b7a76f946>
2009-12-08 10:50:50 +0000

    fix cmf namespace to make it possible to import multiple workflows for the same object
    git-svn-id: http://svn.quintagroup.com/products/quintagroup.transmogrifier/trunk@2490 4df3d6c7-0a05-0410-9bee-ae8b7a76f946

--- quintagroup/transmogrifier/namespaces/cmfns.py
+++ quintagroup/transmogrifier/namespaces/cmfns.py
@@ -13,7 +13,6 @@ class LocalRolesAttribute(base.LocalRolesAttribute):
         return (self.name, 'security')
     
     def processXml(self, context, node):
-        tag, namespace = utils.fixtag(node.tag,context.ns_map)
         nsprefix = node.tag[:node.tag.find('}')+1]
         local_roles = node.findall(nsprefix+self.name)
         
@@ -27,11 +26,52 @@ class LocalRolesAttribute(base.LocalRolesAttribute):
             values.append((lrole.get('user_id'), lrole.get('role')))
         
         return True
+
+class WorkflowAttribute(base.WorkflowAttribute):
     
+    def processXml(self, context, node):
+        data = context.getDataFor(self.namespace.xmlns)
+        wf_data = data.setdefault(self.name, {})
+        nsprefix = node.tag[:node.tag.find('}')+1]
+
+        for wf_node in node.findall(nsprefix+'workflow'):
+            # workflow
+            wf_id = wf_node.attrib.get(nsprefix+'id') or \
+                    wf_node.attrib.get('id')
+            if wf_id is None:
+                continue
+
+            # history
+            wf = wf_data.setdefault(wf_id, [])
+            hist_nodes = wf_node.findall(nsprefix+'history')
+            for hist_node in hist_nodes:
+                record = {}
+                wf.append(record)
+
+                #var
+                var_nodes = hist_node.findall(nsprefix+'var')
+                vid = vtype = value = None
+
+                for var_node in var_nodes:
+                    vid = var_node.attrib.get(nsprefix+'id') or \
+                          var_node.attrib.get('id')
+                    vtype = var_node.attrib.get(nsprefix+'type',None) or \
+                            var_node.attrib.get('type')
+                    value = var_node.attrib.get(nsprefix+'value',None) or \
+                            var_node.attrib.get('value') or ''
+
+                    if not (vid and vtype and value is not None):
+                        continue
+
+                    value = base.demarshall_value(value, vtype)
+                    record[vid] = value
+
+        return True
+
 class CMF(base.CMF):
     
     attributes = (
         base.TypeAttribute('type'),
-        base.WorkflowAttribute('workflow_history '),
+        WorkflowAttribute('workflow_history'),
         LocalRolesAttribute('local_role')
         )
